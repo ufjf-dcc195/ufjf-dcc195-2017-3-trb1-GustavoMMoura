fc0859113cfc654d4716c64ad584e85e852269de
GustavoMMoura <gusta1000@yahoo.com.br>
2017-09-28 20:09:12 -0300

    Configuração Heroku
    Configuração Heroku

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var http = require("http");
 var url = require("url");
-
+var port = process.env.PORT || 3000;
 function start(router, handlers) {
 	http.createServer(function (request, response) {
 		var urlParsed = url.parse(request.url, true);		
 		router.route(urlParsed.pathname, handlers, request, response);		
-	}).listen(3000);
+	}).listen(port);
 	console.log("Servidor rodando em http://localhost:3000");
 }
 
